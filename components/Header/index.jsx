89aa86592ad8b527dd4481a069b9e3f1ccc95896
Jhonleandres <jhonleandres.silva@gmail.com>
2022-04-11 21:43:48 -0300

    first commit

---
+++ components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Index = ({ title }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:title" content={title} key="title" />
+      </Head>
+    </>
+  )
+}
+
+export default Index
---
+++ pages/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const Index = () => {
+  return (
+    <>
+      <Header title="Release Notes" />
+      <div className="bg-white grid justify-items-center p-48">
+        <p className="font-sans font-bold text-5xl">Release Notes</p>
+        <div className="flex flex-row p-10">
+          <div className="">1</div>
+          <div className="">2</div>
+          <div className="">3</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Index

0a68d35c239c90dfa7f0ba7eaa8aa8f20136507b
Jhonleandres <jhonleandres@cessistemas.com.br>
2022-04-12 13:53:47 -0300

    image add

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import Link from 'next/link'
 import Header from '../components/Header'
+import Image from 'next/image'
+import Gestor from '../public/assets/gestor.png'
+import Checkout from '../public/assets/checkout.png'
+import Fluxo from '../public/assets/fluxo.png'
+import Contabil from '../public/assets/contabil.png'
+import Os from '../public/assets/os.png'
+import Vendas from '../public/assets/vendas.png'
+
 
 const Index = () => {
   return (
@@ -8,9 +17,37 @@ const Index = () => {
       <div className="bg-white grid justify-items-center p-48 ">
         <p className="font-sans font-bold text-5xl">Release Notes</p>
         <div className="flex flex-row p-10">
-          <div className="">1</div>
-          <div className="">2</div>
-          <div className="">3</div>
+          <div className="">
+            <Link href={'/gestor'}>
+              <Image alt="Gestor" className='hover:cursor-pointer' src={Gestor} width={100} height={100} />
+            </Link>
+          </div>
+          <div className="">
+            <Link href={'/checkout'}>
+              <Image alt="Checkout" className='hover:cursor-pointer' src={Checkout} width={100} height={100} />
+            </Link>
+          </div>
+          <div className="">
+            <Link href={'/fluxo'}>
+              <Image alt="Fluxo" className='hover:cursor-pointer' src={Fluxo} width={100} height={100} />
+            </Link>
+          </div>
+          <div className="">
+            <Link href={'/vendas'}>
+              <Image alt="Vendas" className='hover:cursor-pointer' src={Vendas} width={100} height={100} />
+            </Link>
+          </div>
+          <div className="">
+            <Link href={'/contabil'}>
+              <Image alt="Contabil" className='hover:cursor-pointer' src={Contabil} width={100} height={100} />
+            </Link>
+          </div>
+          <div className="">
+            <Link href={'/os'}>
+              <Image alt="Os" className='hover:cursor-pointer' src={Os} width={100} height={100} />
+            </Link>
+          </div>
+          
         </div>
       </div>
     </>

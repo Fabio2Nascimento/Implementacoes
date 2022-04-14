e603b993c5d55bfa05ad61aa6a47497ebf37e08d
Jhonleandres <jhonleandres@cessistemas.com.br>
2022-04-14 16:57:03 -0300

    feat: validate date

--- src/pages/api/release/create.js
+++ src/pages/api/release/create.js
@@ -6,9 +6,21 @@ export default async (req, res) => {
     if (req.method == 'POST') {
       //   console.log(req.body)
       const { sistema, date, version, title, description } = req.body
+
+      if (!/^\d{4}\-\d{1,2}\-\d{1,2}$/.test(date)){
+        return res.status(409).json('formato esperado é YYYY-MM-DD')
+      }
+
+      const data = {
+        sistema: sistema.charAt(0).toUpperCase() + sistema.slice(1),
+        date: date,
+        version: version,
+        title: title,
+        description: description.map(des => des)
+      }
       await faunaClient.query(
         q.Create(q.Collection('releases'), {
-          data: req.body
+          data
         })
       )
 

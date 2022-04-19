1a38f0860c2a53d67d9fded879ab7b9596dea35f
Jhonleandres <jhonleandres@cessistemas.com.br>
2022-04-19 12:14:20 -0300

    fix: remove tag Link add get ip locate

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import Header from '../components/Header'
 import Image from 'next/image'
 import Gestor from '../public/assets/gestor.png'
@@ -9,78 +9,98 @@ import Contabil from '../public/assets/contabil.png'
 import Os from '../public/assets/os.png'
 import Vendas from '../public/assets/vendas.png'
 
+
+
+
+
 const Index = () => {
+  const router = useRouter()
+  const lazyRoot = React.useRef(null)
+
+
+  const [ipAddress, setIpAddress] = useState(null);
+  useEffect(() => {
+    const getIp = async () => {
+      fetch('https://api.ipify.org?format=json')
+        .then(res => res.json())
+        .then(data => setIpAddress(data?.ip))
+    }
+    getIp()
+  }, []);
+
+  // console.log(router.events)
+
   return (
     <>
       <Header title='Release Notes' />
-      <div className='bg-white grid justify-items-center p-48 overflow-hidden'>
-        <p className='font-sans font-bold text-5xl'>Release Notes</p>
-        <div className='flex flex-row p-10'>
+      <div className='md:container md:mx-auto bg-white grid justify-items-center p-48 overflow-hidden'>
+        <p className='font-sans font-bold md:text-5xl '>Release Notes</p>
+        <div ref={lazyRoot} className='flex md:flex-row sm:flex-col p-10'>
           <div className=''>
-            <Link href={'/gestor'}>
-              <Image
-                alt='Gestor'
-                className='hover:cursor-pointer'
-                src={Gestor}
-                width={100}
-                height={100}
-              />
-            </Link>
+            <Image
+              onClick={() => { router.push('/gestor') }}
+              lazyRoot={lazyRoot}
+              alt='Gestor'
+              className='hover:cursor-pointer'
+              src={Gestor}
+              width={100}
+              height={100}
+            />
           </div>
           <div className=''>
-            <Link href={'/checkout'}>
-              <Image
-                alt='Checkout'
-                className='hover:cursor-pointer'
-                src={Checkout}
-                width={100}
-                height={100}
-              />
-            </Link>
+            <Image
+              onClick={() => { router.push('/checkout') }}
+              lazyRoot={lazyRoot}
+              alt='Checkout'
+              className='hover:cursor-pointer'
+              src={Checkout}
+              width={100}
+              height={100}
+            />
           </div>
           <div className=''>
-            <Link href={'/fluxo'}>
-              <Image
-                alt='Fluxo'
-                className='hover:cursor-pointer'
-                src={Fluxo}
-                width={100}
-                height={100}
-              />
-            </Link>
+            <Image
+              onClick={() => { router.push('/fluxo') }}
+              lazyRoot={lazyRoot}
+              alt='Fluxo'
+              className='hover:cursor-pointer'
+              src={Fluxo}
+              width={100}
+              height={100}
+            />
           </div>
           <div className=''>
-            <Link href={'/vendas'}>
-              <Image
-                alt='Vendas'
-                className='hover:cursor-pointer'
-                src={Vendas}
-                width={100}
-                height={100}
-              />
-            </Link>
+            <Image
+              onClick={() => { router.push('/vendas') }}
+              lazyRoot={lazyRoot}
+              alt='Vendas'
+              className='hover:cursor-pointer'
+              src={Vendas}
+              width={100}
+              height={100}
+            />
           </div>
           <div className=''>
-            <Link href={'/contabil'}>
-              <Image
-                alt='Contabil'
-                className='hover:cursor-pointer'
-                src={Contabil}
-                width={100}
-                height={100}
-              />
-            </Link>
+            <Image
+              onClick={() => { router.push('/contabil') }}
+              lazyRoot={lazyRoot}
+              alt='Contabil'
+              className='hover:cursor-pointer'
+              src={Contabil}
+              width={100}
+              height={100}
+            />
           </div>
           <div className=''>
-            <Link href={'/os'}>
-              <Image
-                alt='Os'
-                className='hover:cursor-pointer'
-                src={Os}
-                width={100}
-                height={100}
-              />
-            </Link>
+            <Image
+              onClick={() => { router.push('/os') }}
+              lazyRoot={lazyRoot}
+              alt='Os'
+              className='hover:cursor-pointer'
+              src={Os}
+              width={100}
+              height={100}
+            />
           </div>
         </div>
       </div>
